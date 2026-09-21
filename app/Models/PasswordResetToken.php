b4099ed1941cd47956c9f679b57e057295098013
Cássio Santos <maluqueiro@gmail.com>
2026-09-21 03:02:41 -0300

    fix: use dedicated portal password reset table

--- app/Models/PasswordResetToken.php
+++ app/Models/PasswordResetToken.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class PasswordResetToken extends Model
 {
+    protected $table = 'portal_password_reset_tokens';
+
     protected $fillable = ['email', 'token_hash', 'expires_at', 'used_at'];
 
     protected $casts = [
